74e270eb9dae0057ce6e84a941281dcc1f32cda7
Mathias Pius <contact@pius.io>
2023-08-02 21:26:23 +0200

    initial commit

---
+++ src/main.rs
@@ -0,0 +1,274 @@
+use std::collections::HashMap;
+use std::sync::Arc;
+
+use detect::{detect_versions, Dependency};
+use tokio::sync::RwLock;
+use tower_lsp::jsonrpc::Result;
+use tower_lsp::lsp_types::*;
+use tower_lsp::{Client, LanguageServer, LspService, Server};
+
+mod detect;
+
+#[derive(Debug, Clone)]
+struct VersionTracker {
+    pub inner: Arc<RwLock<HashMap<Url, Vec<(usize, Dependency)>>>>,
+}
+
+#[derive(Debug)]
+struct Backend {
+    client: Client,
+    versions: VersionTracker,
+}
+
+#[tower_lsp::async_trait]
+impl LanguageServer for Backend {
+    async fn initialize(&self, _: InitializeParams) -> Result<InitializeResult> {
+        Ok(InitializeResult {
+            server_info: None,
+            capabilities: ServerCapabilities {
+                inlay_hint_provider: Some(OneOf::Left(true)),
+                text_document_sync: Some(TextDocumentSyncCapability::Kind(
+                    TextDocumentSyncKind::FULL,
+                )),
+                completion_provider: Some(CompletionOptions {
+                    resolve_provider: Some(false),
+                    trigger_characters: Some(vec!["=".to_string()]),
+                    work_done_progress_options: Default::default(),
+                    all_commit_characters: None,
+                    ..Default::default()
+                }),
+                execute_command_provider: Some(ExecuteCommandOptions {
+                    commands: vec!["dummy.do_something".to_string()],
+                    work_done_progress_options: Default::default(),
+                }),
+                workspace: Some(WorkspaceServerCapabilities {
+                    workspace_folders: Some(WorkspaceFoldersServerCapabilities {
+                        supported: Some(true),
+                        change_notifications: Some(OneOf::Left(true)),
+                    }),
+                    file_operations: None,
+                }),
+                ..ServerCapabilities::default()
+            },
+            ..Default::default()
+        })
+    }
+
+    async fn initialized(&self, _: InitializedParams) {
+        self.client
+            .log_message(MessageType::INFO, "initialized!")
+            .await;
+    }
+
+    async fn shutdown(&self) -> Result<()> {
+        Ok(())
+    }
+
+    /*
+    async fn did_change_workspace_folders(&self, _: DidChangeWorkspaceFoldersParams) {
+        self.client
+            .log_message(MessageType::INFO, "workspace folders changed!")
+            .await;
+    }
+
+    async fn did_change_configuration(&self, _: DidChangeConfigurationParams) {
+        self.client
+            .log_message(MessageType::INFO, "configuration changed!")
+            .await;
+    }
+
+    async fn execute_command(&self, _: ExecuteCommandParams) -> Result<Option<Value>> {
+        self.client
+            .log_message(MessageType::INFO, "command executed!")
+            .await;
+
+        match self.client.apply_edit(WorkspaceEdit::default()).await {
+            Ok(res) if res.applied => self.client.log_message(MessageType::INFO, "applied").await,
+            Ok(_) => self.client.log_message(MessageType::INFO, "rejected").await,
+            Err(err) => self.client.log_message(MessageType::ERROR, err).await,
+        }
+
+        Ok(None)
+    }
+    */
+
+    async fn did_change_watched_files(&self, _: DidChangeWatchedFilesParams) {
+        self.client
+            .log_message(MessageType::INFO, "watched files have changed!")
+            .await;
+    }
+
+    async fn did_open(&self, _: DidOpenTextDocumentParams) {
+        self.client
+            .log_message(MessageType::INFO, "file opened!")
+            .await;
+    }
+
+    async fn did_change(&self, params: DidChangeTextDocumentParams) {
+        self.client
+            .log_message(MessageType::INFO, "file changed!")
+            .await;
+
+        if let Some(content) = params.content_changes.first() {
+            let dependencies = detect_versions(&content.text);
+
+            self.versions
+                .inner
+                .write()
+                .await
+                .insert(params.text_document.uri, dependencies);
+        }
+    }
+
+    async fn did_save(&self, params: DidSaveTextDocumentParams) {
+        self.client
+            .log_message(MessageType::INFO, "file saved!")
+            .await;
+
+        let dependencies = detect_versions(params.text.as_deref().unwrap_or_default());
+
+        self.versions
+            .inner
+            .write()
+            .await
+            .insert(params.text_document.uri, dependencies);
+    }
+
+    async fn did_close(&self, _: DidCloseTextDocumentParams) {
+        self.client
+            .log_message(MessageType::INFO, "file closed!")
+            .await;
+    }
+
+    async fn completion(&self, _: CompletionParams) -> Result<Option<CompletionResponse>> {
+        Ok(Some(CompletionResponse::Array(vec![
+            CompletionItem::new_simple("Hello".to_string(), "Some detail".to_string()),
+            CompletionItem::new_simple("Bye".to_string(), "More detail".to_string()),
+        ])))
+    }
+
+    async fn inlay_hint(
+        &self,
+        params: tower_lsp::lsp_types::InlayHintParams,
+    ) -> Result<Option<Vec<InlayHint>>> {
+        let uri = &params.text_document.uri;
+
+        self.client
+            .log_message(
+                MessageType::INFO,
+                format!("inlay hint: {uri}, {:#?}", &self.versions),
+            )
+            .await;
+
+        let versions = {
+            let guard = self.versions.inner.read().await;
+            guard.get(&params.text_document.uri).cloned()
+        };
+
+        let Some(versions) = versions else {
+            return Ok(None);
+        };
+
+        self.client
+            .log_message(
+                MessageType::INFO,
+                format!("inlay hint: {uri}, {}", versions.len()),
+            )
+            .await;
+
+        Ok(Some(
+            versions
+                .iter()
+                .filter_map(|(line, dependency)| {
+                    let Some(ref version) = dependency.version else {
+                        return None;
+                    };
+
+                    Some(InlayHint {
+                        text_edits: None,
+                        tooltip: None,
+                        kind: Some(InlayHintKind::TYPE),
+                        padding_left: None,
+                        padding_right: None,
+                        data: None,
+                        position: Position::new(*line as u32, version.end() as u32),
+                        label: InlayHintLabel::LabelParts(vec![InlayHintLabelPart {
+                            value: version.to_string(),
+                            tooltip: None,
+                            location: Some(Location {
+                                uri: params.text_document.uri.clone(),
+                                range: Range {
+                                    start: Position::new(0, 4),
+                                    end: Position::new(0, 5),
+                                },
+                            }),
+                            command: None,
+                        }]),
+                    })
+                })
+                .collect(),
+        ))
+
+        /*
+        let inlay_hint_list = hashmap
+            .into_iter()
+            .map(|(k, v)| {
+                (
+                    k.start,
+                    k.end,
+                    match v {
+                        nrs_language_server::chumsky::Value::Null => "null".to_string(),
+                        nrs_language_server::chumsky::Value::Bool(_) => "bool".to_string(),
+                        nrs_language_server::chumsky::Value::Num(_) => "number".to_string(),
+                        nrs_language_server::chumsky::Value::Str(_) => "string".to_string(),
+                        nrs_language_server::chumsky::Value::List(_) => "[]".to_string(),
+                        nrs_language_server::chumsky::Value::Func(_) => v.to_string(),
+                    },
+                )
+            })
+            .filter_map(|item| {
+                // let start_position = offset_to_position(item.0, document)?;
+                let end_position = offset_to_position(item.1, &document)?;
+                let inlay_hint = InlayHint {
+                    text_edits: None,
+                    tooltip: None,
+                    kind: Some(InlayHintKind::TYPE),
+                    padding_left: None,
+                    padding_right: None,
+                    data: None,
+                    position: end_position,
+                    label: InlayHintLabel::LabelParts(vec![InlayHintLabelPart {
+                        value: item.2,
+                        tooltip: None,
+                        location: Some(Location {
+                            uri: params.text_document.uri.clone(),
+                            range: Range {
+                                start: Position::new(0, 4),
+                                end: Position::new(0, 5),
+                            },
+                        }),
+                        command: None,
+                    }]),
+                };
+                Some(inlay_hint)
+            })
+            .collect::<Vec<_>>();
+
+        */
+    }
+}
+
+#[tokio::main(flavor = "current_thread")]
+async fn main() {
+    tracing_subscriber::fmt().init();
+
+    let (stdin, stdout) = (tokio::io::stdin(), tokio::io::stdout());
+
+    let (service, socket) = LspService::new(|client| Backend {
+        client,
+        versions: VersionTracker {
+            inner: Arc::new(RwLock::new(HashMap::new())),
+        },
+    });
+    Server::new(stdin, stdout, socket).serve(service).await;
+}
